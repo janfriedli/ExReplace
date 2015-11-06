9d8e5e465943a4bd58001f9592f4c59d73a489ba
janfriedli <jan.friedli@gmx.ch>
2015-11-06 16:56:38 +0100

    initial commit

---
+++ replaceExe/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnOpenFile = New System.Windows.Forms.Button()
+        Me.txtFilePath = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtProcessName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.chkAutomaticallyRestart = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(36, 107)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnOpenFile
+        '
+        Me.btnOpenFile.Location = New System.Drawing.Point(422, 37)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(61, 23)
+        Me.btnOpenFile.TabIndex = 1
+        Me.btnOpenFile.Text = "open"
+        Me.btnOpenFile.UseVisualStyleBackColor = True
+        '
+        'txtFilePath
+        '
+        Me.txtFilePath.Location = New System.Drawing.Point(36, 37)
+        Me.txtFilePath.Name = "txtFilePath"
+        Me.txtFilePath.Size = New System.Drawing.Size(380, 20)
+        Me.txtFilePath.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Path to custom exe / file"
+        '
+        'txtProcessName
+        '
+        Me.txtProcessName.Location = New System.Drawing.Point(36, 81)
+        Me.txtProcessName.Name = "txtProcessName"
+        Me.txtProcessName.Size = New System.Drawing.Size(380, 20)
+        Me.txtProcessName.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(190, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Process name which will be interrupted"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'chkAutomaticallyRestart
+        '
+        Me.chkAutomaticallyRestart.AutoSize = True
+        Me.chkAutomaticallyRestart.Location = New System.Drawing.Point(132, 111)
+        Me.chkAutomaticallyRestart.Name = "chkAutomaticallyRestart"
+        Me.chkAutomaticallyRestart.Size = New System.Drawing.Size(127, 17)
+        Me.chkAutomaticallyRestart.TabIndex = 6
+        Me.chkAutomaticallyRestart.Text = "Always restart listener"
+        Me.chkAutomaticallyRestart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(502, 140)
+        Me.Controls.Add(Me.chkAutomaticallyRestart)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtProcessName)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFilePath)
+        Me.Controls.Add(Me.btnOpenFile)
+        Me.Controls.Add(Me.btnStart)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.ShowInTaskbar = False
+        Me.Text = "ExReplacer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnOpenFile As Button
+    Friend WithEvents txtFilePath As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtProcessName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents chkAutomaticallyRestart As CheckBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ replaceExe/Form1.vb
@@ -0,0 +1,93 @@
+﻿Imports System.Threading
+
+
+Public Class Form1
+
+    Declare Function GetAsyncKeyState Lib "user32.dll" (ByVal nVirtKey As Keys) As Short
+
+    Public Const F2 As Long = 113
+    Public Const F4 As Long = 115
+
+    Private Sub ExReplacer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Timer1.Enabled = True
+        Timer1.Interval = 1
+
+    End Sub
+
+    Private Sub interrupt()
+
+        Dim notStarted = True
+
+        If (txtProcessName.Text.Length > 0) Then
+
+            Do While notStarted
+
+                Dim proc As Process() = Process.GetProcessesByName(txtProcessName.Text)
+
+                If (proc.Length > 0) Then
+
+                    'make sure no exception shows up (cannot start) 
+                    Thread.Sleep(50)
+                    proc(0).Kill()
+
+                    If (txtFilePath.Text.Length > 0) Then
+                        Process.Start(txtFilePath.Text)
+                    Else
+                        MsgBox("Please provide a executable path", MsgBoxStyle.Question, "Info")
+                    End If
+
+                    If (Not chkAutomaticallyRestart.Checked = True) Then
+                        notStarted = False
+                    End If
+
+                End If
+            Loop
+
+        Else
+            MsgBox("Please provide a process name", MsgBoxStyle.Question, "Info")
+            ' btnStart.Enabled = True failed wegen thread
+        End If
+
+    End Sub
+
+    Private Sub btnOpenFile_Click(sender As Object, e As EventArgs) Handles btnOpenFile.Click
+
+        If OpenFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Debug.WriteLine(OpenFileDialog1.FileName)
+            txtFilePath.Text = OpenFileDialog1.FileName
+        End If
+
+    End Sub
+
+    Private Async Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        btnStart.Enabled = False
+        btnOpenFile.Enabled = False
+        txtFilePath.Enabled = False
+        txtProcessName.Enabled = False
+
+        'security ;)
+        Me.Hide()
+
+        'waits for the interrupt function to complete
+        Task.Run(Sub()
+                     interrupt()
+                 End Sub)
+
+    End Sub
+
+    'use a shortcut to display the form when it dosen't come up itself
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        If GetAsyncKeyState(F2) <> 0 And GetAsyncKeyState(F4) <> 0 Then
+            btnStart.Enabled = True
+            btnOpenFile.Enabled = True
+            txtFilePath.Enabled = True
+            txtProcessName.Enabled = True
+            Me.Show()
+        End If
+
+    End Sub
+
+End Class
